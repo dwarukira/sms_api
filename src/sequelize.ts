ea48fb8712c808e19c0268aeba891628238d62ef
warukira <dwarukira@gmail.com>
2019-07-26 11:53:28 +0300

    update pool

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -4,8 +4,14 @@ import { Op } from 'sequelize';
 
 const env = process.env.NODE_ENV || 'development';
 
-export const sequelize =  new Sequelize(DB_URI,{
+export const sequelize = new Sequelize(DB_URI, {
   operatorsAliases: Op,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
   modelPaths: [__dirname + "/models"],
   modelMatch: (filename, member) => {
     return filename.substring(0, filename.indexOf('.model')) === member.toLowerCase();
